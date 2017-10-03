e53f77169695c09b82aca179f50a9c21431ee0a6
pvls <pvls@cin.ufpe.br>
2017-10-03 18:59:25 -0300

    scaffold migrado

--- EncostAki/db/migrate/20171002233404_create_users.rb
+++ EncostAki/db/migrate/20171002233404_create_users.rb
@@ -2,11 +2,11 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.number :cpf
+      t.integer :cpf, :limit => 8
       t.string :username
-      t.number :cep
+      t.integer :cep, :limit => 8
       t.string :complement
-      t.number :house_number
+      t.integer :house_number, :limit => 8 
 
       t.timestamps
     end
