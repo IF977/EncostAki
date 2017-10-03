aae698fe2bf18140b2df96243a1cdff57731dc42
pvls <pvls@cin.ufpe.br>
2017-10-03 18:39:49 -0300

    migrate adicionado e bancos dev/test

--- EncostAki/db/schema.rb
+++ EncostAki/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171002233404) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "cpf"
