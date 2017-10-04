228bd2c6799ebf5794360d61fb5e2fdccfc558d4
eacj <eacj@cin.ufpe.br>
2017-10-04 17:02:18 -0300

    Pagina de login adicionada sem rota criada ainda

--- EncostAki/config/routes.rb
+++ EncostAki/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
   get 'home/index'
   root :to => 'home#index'
   resources :locais
+  resources :users
+  resources :login
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
