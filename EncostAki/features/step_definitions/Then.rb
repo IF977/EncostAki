5217ecba7e03de6fac00e135bc4ce6174b286dce
Gustavo <gbl3@cin.ufpe.br>
2017-10-22 22:11:06 -0300

    Incluindo passo para assert sobre conteudo da pagina

--- EncostAki/features/step_definitions/Then.rb
+++ EncostAki/features/step_definitions/Then.rb
@@ -6,11 +6,14 @@ Then(/^I should see Sign in page$/) do
     visit(new_member_session_path)
 end
 
-Then(/^I should see the Cadastro page again$/) do 
+Then(/^I should see the Cadastro page again$/) do
    visit(new_member_registration_path)
 end
 
-Then(/^I should see the Login page again$/) do 
+Then(/^I should see the Login page again$/) do
     visit(new_member_session_path)
 end
 
+Then(/^I should see the text ([^"]*)$/) do |text|
+    expect(page).to have_content(text)
+end
